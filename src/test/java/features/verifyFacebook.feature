2d5a1d0f573c8c223490adc44423551b82a33f12
Nguyen Thi Thanh <nguyen.thi.thanhb@b121451-lt.sun-asterisk.com>
2023-03-13 13:42:32 +0700

    Cucumber 1

---
+++ src/test/java/features/verifyFacebook.feature
@@ -0,0 +1,10 @@
+Feature: Face login page
+  I want to use this template for my feature file
+
+  @displayed
+  Scenario: Verify logins page
+    Given Open facebook aplication
+    Then Verify email textbox displayed
+    And Verify password textbox is display
+    And Close application
+
---
+++ src/test/java/stepDefinitions/facebookSteps.java
@@ -0,0 +1,46 @@
+package stepDefinitions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class facebookSteps {
+	WebDriver driver;
+	
+    @Given("^Open facebook aplication$")
+    public void openFacebookAplication()  {
+    	WebDriverManager.chromedriver().setup();
+    	driver = new ChromeDriver();
+    	driver.get("https://www.facebook.com/");
+    	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        
+    }
+
+    @Then("^Verify email textbox displayed$")
+    public void verifyEmailTextboxDisplayed()  {
+    	Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
+        
+    }
+
+    @And("^Verify password textbox is display$")
+    public void verifyPasswordTextboxIsDisplay()  {
+    	Assert.assertTrue(driver.findElement(By.id("pass")).isDisplayed());
+        
+    }
+    
+    @And("^Close application$")
+    public void CloseApplication()  {
+    	driver.quit();;
+        
+    }
+
+
+}
